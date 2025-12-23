93a783366fc61e8d7255986503fecaaad7280d6a
julien-kap <jgooris@kap.bzh>
2025-12-23 20:10:57 +0100

    For loop approch for aggregations

--- dev_test.py
+++ dev_test.py
@@ -1,8 +1,11 @@
 from functools import reduce
 from polars import (
     DataFrame,
+    LazyFrame,
     col,
     Datetime,
+    date_ranges,
+    datetime_ranges,
     int_range,
     len as length,
     Float64,
@@ -15,6 +18,7 @@ from polars import (
     len as pl_len,
     selectors as cs,
     concat,
+    map_batches,
 )
 from datetime import datetime, timezone, timedelta
 from zoneinfo import ZoneInfo
@@ -32,10 +36,8 @@ from mlweather.aggregation import (
 load_dotenv()
 
 # FETCH OBSERVATIONS ##############################################################
-start_time = datetime.now(timezone.utc)
-
-start_dt = datetime(2024, 5, 1, 0, 0, 0, tzinfo=timezone.utc)
-end_dt = datetime(2024, 6, 1, 0, 0, 0, tzinfo=timezone.utc)
+start_dt = datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+end_dt = start_dt + timedelta(days=366)
 
 # Collect data
 obs = Observations.collect(
@@ -46,8 +48,7 @@ obs = Observations.collect(
     ],
     start_dt,
     end_dt,
-    # api_key=os.getenv("OPENMETEO_API_KEY"),
-    api_key=os.getenv("WEATHERAPI_API_KEY"),
+    api_key=os.getenv("OPENMETEO_API_KEY"),
     verbose=False,
 )
 
@@ -60,22 +61,217 @@ fore = Forecasts.collect(
     start_dt,
     end_dt,
     (0, 7),
-    api_key=os.getenv("WEATHERAPI_API_KEY"),
+    api_key=os.getenv("OPENMETEO_API_KEY"),
 )
 
-# Insert forecast path into all observations entries to replicate forecast path by hour (not only dayly init_times).
-# Do so by merging the forecast path (based on midnight computation, not hourly) with the observations based on the next day midnight into each observation valid_datetime.
-left_side = (
-    obs.record_table.sort("valid_datetime")
-    .with_row_index()
-    # Create a synthétic init_time on the obs side to inject the forecast paths
-    .with_columns(
-        (col("valid_datetime").dt.truncate("1d") + timedelta(days=1)).alias(
-            "init_datetime@fore"
+def _prepare_records(obs: Observations | None, fore: Forecasts | None, *, lazy_output: bool) -> DataFrame | LazyFrame:
+    # Replicate the forecast data to hourly frequency by self merging on the hourly spread dates
+    # Add hours to dates based on init_datetime
+    # Insert date ranges (hours within the day) withi the cells of a new column then explode
+    non_meteo_vars = [
+        "init_datetime",
+        "valid_datetime",
+        "past_day",
+        "init_datetime_raw",
+        "init_datetime_raw_end",
+    ]
+
+    if obs is None:
+        obs_side = DataFrame().lazy()
+    else:
+        # Add id to observations
+        obs_side = (
+            obs.record_table.lazy().sort("valid_datetime")
+            .with_row_index("id")
+            .with_columns("observation-" + col("id").cast(str).alias("id"))
         )
+
+    if fore is None:
+        forecast_side = DataFrame().lazy()
+    else:
+        forecast_side = (
+            # Add id
+            fore.record_table.lazy().with_row_index("id")
+            .with_columns( "forecast-" + col("id").cast(str).alias("id"))
+            # Replicate the  daily computeed forecast to intraday steps (hourly)
+            .rename({"init_datetime": "init_datetime_raw"})
+            # Generate datetime ranges for each init_datetime (24 hour with the daily init_datetime)
+            .with_columns(
+                (col("init_datetime_raw") + timedelta(hours=23)).alias("init_datetime_raw_end")
+            )
+            .with_columns(
+                datetime_ranges(
+                    "init_datetime_raw", "init_datetime_raw_end", interval=timedelta(hours=1)
+                ).alias("init_datetime")
+            )
+            # From compact (nested) form to long form
+            .explode("init_datetime")
+            .select(*non_meteo_vars, cs.exclude(non_meteo_vars))
+            .sort(["valid_datetime", "init_datetime"])
+        )
+
+
+    # Assemble
+    all_records = concat([obs_side, forecast_side], how="diagonal")
+    # Treat non lazy
+    if not lazy_output:
+        all_records = all_records.collect()
+
+    return all_records
+
+def _filter_apply(
+    all_records: LazyFrame | DataFrame,
+    focal_valid_datetime: datetime,
+    observation_period: timedelta | None,
+    forecast_period: timedelta | None,
+) -> LazyFrame | DataFrame:
+    # Deal with 0 length periods
+    # Check that at least one period is not none
+    if observation_period is None and forecast_period is None:
+        raise ValueError(
+            "At least one of observation_period or forecast_period must be not None"
+        )
+    if observation_period is None:
+        observation_period = timedelta(0)
+    if forecast_period is None:
+        forecast_period = timedelta(0)
+    return (
+        all_records.filter(
+        # In obs:
+        # from the valid_time - the forecast period (included),
+        # back to included to the observation period and forecast period (bound excluded)
+        (
+            col("init_datetime").is_null()      # only for observations
+            & (col("valid_datetime") <= (focal_valid_datetime - forecast_period))
+            & (
+                col("valid_datetime")
+                > (focal_valid_datetime - forecast_period - observation_period)
+            )
+        ) |
+        # In forecast:
+        # for the init_times that match perfectly the first valid time
+        # from the valid_time (included),
+        # back to included to the observation period period (bound excluded)
+        (   
+            col("init_datetime").is_not_null()      # only for forecasts
+            & (col("init_datetime") ==(focal_valid_datetime - forecast_period))
+            & (col("valid_datetime") <= (focal_valid_datetime))
+            & (
+                col("valid_datetime")
+                > (focal_valid_datetime - forecast_period)
+            )
+        )
+    ).with_columns(
+        lit(focal_valid_datetime).alias("focal_valid_datetime"),
+        # Number of rows in the output
+        length().alias("n_rows_in_filtered")
     )
-    .rename(lambda x: x + "@obs" if x != "init_datetime@fore" else x)
-).with_columns(lit(1).alias("test@obs"))
+    .sort(["valid_datetime", "init_datetime"])
+    )
+
+
+all_records = _prepare_records(obs, fore, lazy_output=True)
+
+start_timer = datetime.now(timezone.utc)
+
+def get_focal_valid_datetimes(all_records: LazyFrame | DataFrame) -> list[datetime]:
+    # Restrict the usefull serie (compatible with lazy mode)
+    focal_valid_datetimes = (
+        all_records
+        .select("valid_datetime")
+        .filter(col("valid_datetime").is_not_null())
+        .sort("valid_datetime")
+        .unique()
+    )
+    if isinstance(focal_valid_datetimes, LazyFrame):
+        focal_valid_datetimes = focal_valid_datetimes.collect()
+
+    return focal_valid_datetimes["valid_datetime"].to_list()
+
+get_focal_valid_datetimes(all_records)
+
+concat(
+    map(
+        lambda dt: _filter_apply(
+                all_records,
+                dt,
+                timedelta(hours=2),
+                timedelta(hours=2),
+        ),
+        get_focal_valid_datetimes(all_records)
+    )
+)
+
+for dt in get_focal_valid_datetimes(all_records):
+    _filter_apply(
+        all_records,
+        dt,
+        timedelta(hours=2),
+        timedelta(hours=2),
+    )
+print("Timer:", datetime.now(timezone.utc) - start_timer)
+
+start_timer = datetime.now(timezone.utc)
+concatenated = concat(collected_results, how="diagonal")
+if isinstance(all_records, LazyFrame):
+    concatenated = concatenated.collect()
+print("Timer:", datetime.now(timezone.utc) - start_timer)
+
+all_records.collect()
+all_records = all_records.drop("init_datetime_raw", "init_datetime_raw_end", "past_day")
+
+
+ len(
+    all_records_lazy.select("valid_datetime")
+    .filter(col("valid_datetime").is_not_null())
+    .unique()
+    .collect()["valid_datetime"]
+    .to_list()
+)
+
+
+for valid_datetime in all_records["valid_datetime"].unique():
+    all_records.filter(
+        (col("init_datetime").is_null()) & (col("valid_datetime") < valid_datetime)
+    )
+
+
+(
+    filter_past_weather(
+        pw_agg,
+        agg_conf.measure_name,
+        # At the end of the first duration period
+        dt - duration[2] - Hour(1),
+        duration[1],
+    ),
+)
+met_vars = ["precipitation", "temperature_2m"]
+test = (
+    all_records.sort("valid_datetime", "init_datetime")
+    .rolling(
+        index_column="valid_datetime",
+        period=timedelta(hours=8),
+    )
+    .agg(
+        col("id").alias("ids_in_window"),
+        col("valid_datetime").min().alias("min_valid_datetime"),
+        col("valid_datetime").max().alias("max_valid_datetime"),
+        pl_len().alias("length"),
+        # # Return the values within the rolling window as a list
+        *[col(met_var).alias(f"{met_var}_values_in_window") for met_var in met_vars],
+        col("valid_datetime").alias("valid_datetimes_in_window"),
+        col("init_datetime").alias("init_datetimes_in_window"),
+    )
+    .with_row_index("agg_group_id")
+    .explode(
+        [
+            "ids_in_window",
+            "valid_datetimes_in_window",
+            "init_datetimes_in_window",
+            *[f"{met_var}_values_in_window" for met_var in met_vars],
+        ]
+    )
+)
 
 # Add suffix to right side columns
 right_side = (
