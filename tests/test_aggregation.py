980b50332d5324217d8495ee4c7921dcef2fc40d
julien-kap <jgooris@kap.bzh>
2026-06-25 21:18:08 +0200

    changed test for 0 duration periods + added the license

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -27,9 +27,7 @@ class TestAggregation:
     def test_get_steps_in_agg_periods(self):
         # Check the number of steps in aggregation periods
         assert Aggregation.get_steps_in_agg_periods(timedelta(hours=24)) == 24
-        # Error if negative or zero period
-        with pytest.raises(ValueError, match="must be equal or greater than zero"):
-            Aggregation.get_steps_in_agg_periods(timedelta(hours=0))
+        # Error if negative
         with pytest.raises(ValueError, match="must be equal or greater than zero"):
             Aggregation.get_steps_in_agg_periods(timedelta(hours=-1))
 
